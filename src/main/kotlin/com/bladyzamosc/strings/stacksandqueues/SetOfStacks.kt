26b093887d38bcaa4fe8e3cda0b7e35481cf6596
bladyzamosc <bladyzamosc@gmail.com>
2022-09-04 13:12:19 +0200

    Set of stacks

---
+++ src/main/kotlin/com/bladyzamosc/strings/stacksandqueues/SetOfStacks.kt
@@ -0,0 +1,43 @@
+package com.bladyzamosc.strings.stacksandqueues
+
+import java.util.*
+
+/**
+ * User: Z6EKI
+ * Date: 04.09.2022
+ */
+class SetOfStacks(private var capacity: Int) {
+
+    private var stacks: MutableSet<Stack<Int>> = mutableSetOf()
+
+    fun push(i: Int) {
+        if (stacks.isEmpty()) {
+            stacks.add(Stack())
+        }
+        val overflow = stacks.last().size == capacity
+        if (overflow) {
+            stacks.add(Stack())
+        }
+        stacks.last().push(i)
+    }
+
+    fun peek(): Int {
+        return stacks.last().peek()
+    }
+
+    fun noSubStacks(): Int {
+        return stacks.size
+    }
+
+    fun size(): Int {
+        return stacks.sumOf { a -> a.size }
+    }
+
+    fun pop(): Int {
+        val result = stacks.last().pop()
+        if (stacks.last().size == 0) {
+            stacks.remove(stacks.last())
+        }
+        return result
+    }
+}
---
+++ src/test/kotlin/com/bladyzamosc/stacksandqueues/SetOfStacksTest.kt
@@ -0,0 +1,32 @@
+package com.bladyzamosc.stacksandqueues
+
+import com.bladyzamosc.strings.stacksandqueues.SetOfStacks
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+/**
+ * User: Z6EKI
+ * Date: 04.09.2022
+ */
+class SetOfStacksTest {
+
+    @Test
+    fun test2Stacks() {
+        val capacity = 5
+        val max = 8
+        val setOfStack = SetOfStacks(capacity);
+
+        for (i in 1..max) {
+            setOfStack.push(i)
+        }
+
+        Assertions.assertEquals(max, setOfStack.peek())
+        Assertions.assertEquals(2, setOfStack.noSubStacks())
+        Assertions.assertEquals(8, setOfStack.size())
+        Assertions.assertEquals(max, setOfStack.pop())
+        Assertions.assertEquals(7, setOfStack.size())
+    }
+
+}
